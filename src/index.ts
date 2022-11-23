245ea46942002685d78c138f10409cb976ec2391
Inês Soares Silva <inesoaresilva@gmail.com>
2022-11-23 11:09:07 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ import lodash from "lodash";
 
 console.log(lodash.merge({ a: 1 }, { b: 2 }));
 console.log(lodash.merge({ a: 1 }, { b: 2 }));
+
+console.log(lodash.merge({ a: 1 }, { b: 2 }));
+console.log(lodash.merge({ a: 1 }, { b: 2 }));
